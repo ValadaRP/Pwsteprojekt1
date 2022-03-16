c6c40a51db215f0301027ff21d44101d85221301
ValadaRP <s37214@s.pwste.edu.pl>
2022-03-16 17:43:58 +0100

    Dodanie intencji

--- app/src/main/java/com/example/pwsteprojekt/MainActivity.java
+++ app/src/main/java/com/example/pwsteprojekt/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.pwsteprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button_usterka;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_usterka = findViewById(R.id.button_usterka);
+
+        button_usterka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Usterka.class);
+                startActivity(intent);
+            }
+        });
     }
 }
