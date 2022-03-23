20a925d85d001d49a732f924e4753771bc39875a
aleksandra <s37235@s.pwste.edu.pl>
2022-03-23 12:25:45 +0100

    dodanie trybu ciemnego / jasnego do ustawien

--- app/src/main/java/com/example/pwsteprojekt/MainActivity.java
+++ app/src/main/java/com/example/pwsteprojekt/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pwsteprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
