df445f5e70fdb791b0ab7e1908567390fbf9e385
ValadaRP <s37214@s.pwste.edu.pl>
2022-03-23 17:41:18 +0100

    Zmiany mapy

--- app/src/main/java/com/example/pwsteprojekt/Mapy.java
+++ app/src/main/java/com/example/pwsteprojekt/Mapy.java
@@ -2,13 +2,33 @@ package com.example.pwsteprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mapy extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapy);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=Jarosław,+Poland&mode=d");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+
+            }
+        });
     }
 }
