d3271bbef860cfaa4c48ae8a3fdc8289d9678a41
Mateusz <mateusz7241@gmail.com>
2022-03-29 21:24:00 +0200

    aktywnosc wiadomosci, deklaracja editText i Button

--- app/src/main/java/com/example/pwsteprojekt/Wiadomosci.java
+++ app/src/main/java/com/example/pwsteprojekt/Wiadomosci.java
@@ -3,14 +3,20 @@ package com.example.pwsteprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Wiadomosci extends AppCompatActivity {
 
-
+    private Button wyslijButton;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiadomosci);
+        wyslijButton = findViewById(R.id.wyslijButton);
+        editTextMessage = findViewById(R.id.editTextMessage);
+
     }
 }
