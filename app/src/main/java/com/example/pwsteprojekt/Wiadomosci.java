65027cfb17768b31e0f786c1f161545879be6567
Mateusz <mateusz7241@gmail.com>
2022-04-01 11:00:00 +0200

    Wiadomosci dodanie listy

--- app/src/main/java/com/example/pwsteprojekt/Wiadomosci.java
+++ app/src/main/java/com/example/pwsteprojekt/Wiadomosci.java
@@ -3,20 +3,36 @@ package com.example.pwsteprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Wiadomosci extends AppCompatActivity {
 
-    private Button wyslijButton;
-    private EditText editTextMessage;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private TextView infoW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiadomosci);
-        wyslijButton = findViewById(R.id.wyslijButton);
-        editTextMessage = findViewById(R.id.editTextMessage);
 
+        list = (ListView) findViewById(R.id.list);
+        infoW = findViewById(R.id.infoW);
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+        String[] wiadomosci = {};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mobileArray);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this::onItemClick);
+
+    }
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        String wiadomosc = list.getItemAtPosition(position).toString();
+        infoW.setText(wiadomosc);
     }
 }
