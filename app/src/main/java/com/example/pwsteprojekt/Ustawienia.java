abcfbcbf4e9254a5bdf5b69b6eccbfc59e4bb2f8
aleksandra <s37235@s.pwste.edu.pl>
2022-04-24 13:14:51 +0200

    dodanie trybu ciemnego / jasnego do ustawien

--- app/src/main/java/com/example/pwsteprojekt/Ustawienia.java
+++ app/src/main/java/com/example/pwsteprojekt/Ustawienia.java
@@ -1,29 +1,32 @@
 package com.example.pwsteprojekt;
 
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class Ustawienia extends AppCompatActivity {
     private Switch aSwitch;
-    private ConstraintLayout view1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ustawienia);
 
         aSwitch = findViewById(R.id.aSwitch);
-        view1 = findViewById(R.id.view1);
 
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if(isChecked){
                     aSwitch.setText("Tryb ciemny");
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
                 }else{
                     aSwitch.setText("Tryb jasny");
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
             }
         });
